866e8f90eda7869154c976a711ffbd58f3c9be57
Sengly456 <senglyou84@gmail.com>
2025-10-20 11:19:34 +0700

    Close #7 landing page and navbar

---
+++ app/components/FeatureCard.vue
@@ -0,0 +1,31 @@
+<template>
+
+
+    <div class=" w-full md:w-[270px] lg:w-[270px] h-[160px] md:h-[230px] border border-[#CFD1E8] rounded-[8px] p-[16px] ">
+      <Icon  :name="icon" class="w-[24px] h-[24px] mb-[12px] " />
+      <p class="font-bold w-full md:w-[189px] text-[16px] leading-[20px] mb-[4px] text-wrap ">{{ title }}</p>
+
+      <p class="text-[16px] w-full md:w-[189px] text-[#47579E] text-wrap">{{ description }}</p>
+    </div>
+
+
+</template>
+<script setup>  
+defineProps({
+  icon:({
+    type:String,
+    required:true,
+  }),
+  title:({
+    type:String,
+    required:true,
+  }),
+  description: ({
+    type:String,
+    required:true,
+  })
+});
+
+
+
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon"],
   app: {
     head: {
       htmlAttrs: { "data-theme": "light" }, // default light mode
