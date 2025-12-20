1799cee996f60e436b07ff7a1d9d811f93249008
Nangdy Panhar <181545922+nangdypanhar@users.noreply.github.com>
2025-12-20 20:25:03 +0700

    Fix Base API (#53)

--- app/core/api/base-api.ts
+++ app/core/api/base-api.ts
@@ -1,26 +1,22 @@
 import { useCookie, navigateTo } from "#app";
 
-export const baseApi = async <T = unknown>(
+export const baseApi = async <T>(
   endpoint: string,
   options: Record<string, any> = {}
 ): Promise<T> => {
   const config = useRuntimeConfig();
-  const token = useCookie("access_token").value;
+  const token = process.client ? useCookie("access_token").value : null;
 
-  const request = () =>
-    $fetch<T>(`${config.public.apiBase}${endpoint}`, {
+  try {
+    return await $fetch<T>(`${config.public.apiBase}${endpoint}`, {
       ...options,
       headers: {
         ...(options.headers || {}),
-        Authorization: token ? `Bearer ${token}` : undefined,
+        ...(token ? { Authorization: `Bearer ${token}` } : {}),
       },
     });
-
-  try {
-    return await request();
   } catch (err: any) {
-    if (err?.status === 401) {
-      console.warn("Unauthorized - token invalid");
+    if (err?.status === 401 || err?.status === 403) {
       useCookie("access_token").value = null;
       return navigateTo("/login") as any;
     }
